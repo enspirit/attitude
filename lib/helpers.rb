70225fb8fc575c1da52c12debba74a469baacbfa
Bernard Lambeau <blambeau@gmail.com>
2012-06-17 12:38:11 +0200

    Remove stuff provided by polygon itself.

--- lib/helpers.rb
+++ lib/helpers.rb
@@ -1,18 +1,2 @@
 module Helpers
-
-  def sitemap_locals
-    { :urls => database.sitemap }
-  end
-
-  def page_locals(path = "")
-    rel = lispy do
-      (unwrap \
-        (extend (restrict :sitemap, :path => path),
-                :data => lambda{ entry.to_hash }),
-        :data)
-    end
-    rel && rel.to_a.first
-  end
-  alias :index_locals :page_locals
-
 end
