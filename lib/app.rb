bba16b2fca589a1678c1d11986410d01df8cd525
Bernard Lambeau <blambeau@gmail.com>
2012-07-18 09:57:50 +0200

    Add redirect rules for indexed old pages.

--- lib/app.rb
+++ lib/app.rb
@@ -21,6 +21,13 @@ class App < Polygon::Base
     redirect '/balletschool/'
   end
 
+  get '/uurrooster.htm' do redirect '/balletstudio/uurrooster/'  end
+  get '/optredens.htm'  do redirect '/voorstellingen/'           end
+  get '/historiek.htm'  do redirect '/balletschool/'             end
+  get '/dansen.htm'     do redirect '/dansrichtingen/'           end
+  get '/klassen.htm'    do redirect '/dansrichtingen/'           end
+  get '/ingrid.htm'     do redirect '/docenten/ingrid-fonteyne/' end
+
   get %r{/(.*)} do
     path = params[:captures].first || 'balletschool'
     if locals = page_locals(path)
